78ea192594fff0789ee470a8721c9b6944e2bfa8
Andriy <andriy.rom1294@gmail.com>
2017-11-16 13:11:02 +0200

    Added data structures without implementation.

--- Kmeans/Kmeans.py
+++ Kmeans/Kmeans.py
@@ -4,6 +4,21 @@ Created on Thu Nov 16 10:51:40 2017
 
 @author: Andriy
 """
+import numpy as np
 
+class KMeans:
+    #Points = ndarray[["x", "y"],["x", "y"],...]
+    def __init__(self, points):
+        self.__points = points.copy()
+    
+    def get_clasters(self, count):
+        return [Claster()]
+    
+class Claster:
+    #Center = ndarray["x", "y"]
+    #Points = ndarray[["x", "y"],["x", "y"],...]
+    def __init__(self, center=None, points=None):
+        self.center = center
+        self.points = points
         
         
--- Kmeans/KmeansTest.py
+++ Kmeans/KmeansTest.py
@@ -5,3 +5,19 @@ Created on Thu Nov 16 12:43:09 2017
 @author: Andriy
 """
 
+import unittest
+import numpy as np
+from Kmeans import KMeans
+
+class KmeansTest(unittest.TestCase):
+    
+    def testClasterPartition(self):
+        points = np.array([[1,2],[2,2], [3,1], [0,-5], [1,-7], [3, -6]])
+        alg = KMeans(points)
+        result = alg.get_clasters(1)
+        
+        self.assertEqual(len(result), 1)
+        np.testing.assert_allclose(result[0].points, points)
+        
+
+unittest.main()
