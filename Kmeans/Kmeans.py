b5eff3ae24b57f646e9e5ebeb771522b22c97279
Andriy <andriy.rom1294@gmail.com>
2017-11-16 12:51:54 +0200

    Initial commit

---
+++ Kmeans/Kmeans.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 10:51:40 2017
+
+@author: Andriy
+"""
+
+        
+        
---
+++ Kmeans/KmeansTest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 12:43:09 2017
+
+@author: Andriy
+"""
+
