b900f2589c1222f6bcc5901b4145cb1ac799d8a5
Andriy <andriy.rom1294@gmail.com>
2017-11-17 20:10:45 +0200

    Changed points type to numpy.array implemented gradient calculation.

--- GradientDescent/GradientDescent.py
+++ GradientDescent/GradientDescent.py
@@ -8,9 +8,19 @@ Created on Fri Nov 17 15:35:48 2017
 import numpy as np
 
 class GradientDescent:
-    
+    # function -> f(x), where x - numpy.array
     def __init__(self, function):
+        self.__step = 0.02
         self.__func = function
     
     def optimize(self, start_point):
-        pass
+        gradient = self.calc_grad(start_point)
+        return np.array([-1])
+    
+    def calc_grad(self, point):
+        p_count = point.size
+        new_points = np.eye(p_count) * self.__step + point
+        new_points_values = np.apply_along_axis(self.__func, 1, new_points)
+        partial_gains = new_points_values - self.__func(point)
+        gradient = partial_gains / self.__step
+        return gradient
--- GradientDescent/GradientDescentTest.py
+++ GradientDescent/GradientDescentTest.py
@@ -12,11 +12,27 @@ from GradientDescent import  GradientDescent as grad
 class GradientDescentTest(unittest.TestCase):
     
     def testParabola(self):
-        f = lambda x: x**2
+        f = lambda x: x[0]**2
         opt = grad(f)
-        res = opt.optimize(5)
-        expected = 0
+        res = opt.optimize(np.array([5]))
+        expected = np.array([0])
         np.testing.assert_allclose(res,expected, 1e-3)
-
+    
+    def testParabolaGradient(self):
+        points = np.array([[0],[1],[-1]])
+        f = lambda x: x[0]**2
+        opt = grad(f)
+        
+        res = opt.calc_grad(points[0])
+        expected = np.array([0.02])
+        np.testing.assert_allclose(res,expected, 1e-2)
+        
+        res = opt.calc_grad(points[1])
+        expected = np.array([2.02])
+        np.testing.assert_allclose(res,expected, 1e-2)
+        
+        res = opt.calc_grad(points[2])
+        expected = np.array([-1.98])
+        np.testing.assert_allclose(res,expected, 1e-2)
 
 unittest.main()        
