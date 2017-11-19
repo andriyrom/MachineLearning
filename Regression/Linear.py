4e2e3568fe0bcfc0b20d75df42b9cf11054d3a6c
Andriy <andriy.rom1294@gmail.com>
2017-11-19 15:55:26 +0200

    Implemented Linear regression with custom gradient calculation in class LinearCustom.

--- Regression/Linear.py
+++ Regression/Linear.py
@@ -45,3 +45,50 @@ class Linear:
             x_ext = self.__extend_x(x)
             return x_ext.dot(params)
         return f
+    
+class LinearCustom:
+    #x_vals - numpy.array, dim = 2; y_vals - numpy.array, dim=1 
+    def __init__(self, x_vals, y_vals):
+        self.__x = self.__extend_x(x_vals)
+        self.__y = y_vals.copy()
+    
+    def __extend_x(self, x_vals):
+        ones_arr = np.ones((x_vals.shape[0], 1))
+        return np.hstack((ones_arr, x_vals))
+    
+    def regression(self):
+        params_count = self.__x.shape[1]
+        init_vals = self.__init_params(params_count)
+        params = self.__minimize(init_vals, 1e-3, 0.007, 0.09)
+        return self.__lin_func(params)
+    
+    def __minimize(self, init_vals, tolerance, learn_rate, mom_rate):
+        point = init_vals.astype("float64")
+        gradient = self.__gradient(point)
+        momentum = gradient * mom_rate
+        new_step = True
+        while (new_step):
+            momentum = momentum * mom_rate + learn_rate * gradient
+            point -= momentum
+            gradient = self.__gradient(point)
+            new_step = np.abs(gradient).max() > tolerance
+        return point
+    
+    def __gradient(self, params):
+        diffs = self.__x.dot(params) - self.__y
+        grad = np.zeros(params.size)
+        for index in range(params.size):
+            column = self.__x[:,index]
+            derivative = np.sum(column * diffs)
+            grad[index] = derivative
+        return grad
+    
+    def __init_params(self, count):
+        raw_vals = np.random.rand(count)
+        return raw_vals/raw_vals.sum()
+    
+    def __lin_func(self, params):
+        def f(x):
+            x_ext = self.__extend_x(x)
+            return x_ext.dot(params)
+        return f
--- Regression/LinearTest.py
+++ Regression/LinearTest.py
@@ -7,7 +7,7 @@ Created on Sat Nov 18 16:41:14 2017
 
 import unittest
 import numpy as np
-from Linear import Linear
+from Linear import Linear, LinearCustom
 
 class LinearTest(unittest.TestCase):
     def lin_func(self, x, params):
@@ -21,6 +21,25 @@ class LinearTest(unittest.TestCase):
         x_learn = np.linspace(-10, 10, samples_count).reshape((samples_count, 1))
         y_learn = self.lin_func(x_learn, params)
         lin_alg = Linear(x_learn,y_learn)
+        reg_func = lin_alg.regression()
+        
+        samples_count = 10
+        x_test = np.linspace(20, 27, samples_count).reshape((samples_count, 1))
+        y_test = self.lin_func(x_test, params)
+        y_res = reg_func(x_test)
+        np.testing.assert_allclose(y_res,y_test, atol=1e-3)
+        
+        x_test = np.linspace(-42, -35, samples_count).reshape((samples_count, 1))
+        y_test = self.lin_func(x_test, params)
+        y_res = reg_func(x_test)
+        np.testing.assert_allclose(y_res,y_test, atol=1e-3)
+    
+    def testOneParamCustomRegression(self):
+        params = np.array([1, -0.5])
+        samples_count = 5
+        x_learn = np.linspace(-10, 10, samples_count).reshape((samples_count, 1))
+        y_learn = self.lin_func(x_learn, params)
+        lin_alg = LinearCustom(x_learn,y_learn)
         reg_func = lin_alg.regression()
         
         samples_count = 10
