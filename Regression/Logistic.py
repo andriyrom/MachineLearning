e363a45c5e33ad8e4e62f3335be0c4911d1d626f
Andriy <andriy.rom1294@gmail.com>
2017-11-20 18:14:46 +0200

    Implemented Logistic regression with custom gradient calculation.

--- Regression/Logistic.py
+++ Regression/Logistic.py
@@ -48,3 +48,50 @@ class Logistic:
             z = self.__extend_x(x).dot(params)
             return sigmoid(z)
         return f
+
+class LogisticCustom:
+    #x_vals - numpy.array, dim = 2; y_vals - numpy.array, dim=1 
+    def __init__(self, x_vals, y_vals):
+        self.__x = self.__extend_x(x_vals)
+        self.__y = y_vals.copy()
+    
+    def __extend_x(self, x_vals):
+        ones_arr = np.ones((x_vals.shape[0], 1))
+        return np.hstack((ones_arr, x_vals))
+    
+    def regression(self):
+        params_count = self.__x.shape[1]
+        init_vals = self.__init_params(params_count)
+        params = self.__minimize(init_vals, 0.07, 0.03, 0.2)
+        return self.__log_func(params)
+    
+    def __minimize(self, init_vals, tolerance, learn_rate, mom_rate):
+        point = init_vals.astype("float64")
+        gradient = self.__gradient(point)
+        momentum = gradient * mom_rate
+        new_step = True
+        while (new_step):
+            momentum = momentum * mom_rate + learn_rate * gradient
+            point -= momentum
+            gradient = self.__gradient(point)
+            new_step = np.abs(gradient).max() > tolerance
+        return point
+    
+    def __gradient(self, params):
+        diffs = sigmoid(self.__x.dot(params)) - self.__y
+        grad = np.zeros(params.size)
+        for index in range(params.size):
+            column = self.__x[:,index]
+            derivative = np.sum(column * diffs)
+            grad[index] = derivative
+        return grad
+    
+    def __init_params(self, count):
+        raw_vals = np.random.rand(count)
+        return raw_vals/raw_vals.sum()
+    
+    def __log_func(self, params):
+        def f(x):
+            z = self.__extend_x(x).dot(params)
+            return sigmoid(z)
+        return f
--- Regression/LogisticTest.py
+++ Regression/LogisticTest.py
@@ -6,7 +6,7 @@ Created on Sat Nov 19 15:58:16 2017
 """
 import unittest
 import numpy as np
-from Logistic import Logistic
+from Logistic import Logistic, LogisticCustom
 
 class LogisticTest(unittest.TestCase):
     def lin_func(self, x, params):
@@ -34,6 +34,38 @@ class LogisticTest(unittest.TestCase):
         x_learn = np.array([[-5,7], [-4, 5], [-1, 12], [0, 11], [3, -1], [7, -10], [4, -6], [12, -7]])
         y_learn = np.array([0, 0, 0, 0, 1, 1, 1, 1])
         lin_alg = Logistic(x_learn,y_learn)
+        reg_func = lin_alg.regression()
+        
+        x_test = np.array([[-6, 12], [-10, 10], [-7, 11], [-42, 21],[-1.5, 15], [-2, 23]])
+        y_test = np.array([0, 0, 0, 0, 0, 0])
+        y_res = reg_func(x_test)
+        np.testing.assert_allclose(y_res,y_test, atol=1e-2)
+        
+        x_test = np.array([[6, -5], [15, -5], [21, -12], [42, 0], [3.3, -7], [70, 2]])
+        y_test = np.array([1, 1, 1, 1, 1, 1])
+        y_res = reg_func(x_test)
+        np.testing.assert_allclose(y_res,y_test, atol=1e-3)
+    
+    def testOneParamLogisticCustom(self):
+        x_learn = np.array([[-5], [-4], [-1], [0],[3], [7], [4], [12]])
+        y_learn = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+        lin_alg = LogisticCustom(x_learn,y_learn)
+        reg_func = lin_alg.regression()
+
+        x_test = np.array([[-6], [-10], [-7], [-42],[-1.5], [-2]])
+        y_test = np.array([0, 0, 0, 0, 0, 0])
+        y_res = reg_func(x_test)
+        np.testing.assert_allclose(y_res,y_test, atol=1e-2)
+        
+        x_test = np.array([[6], [15], [21], [42],[3.3], [70]])
+        y_test = np.array([1, 1, 1, 1, 1, 1])
+        y_res = reg_func(x_test)
+        np.testing.assert_allclose(y_res,y_test, atol=1e-2)
+        
+    def testTwoParamLogisticCustom(self):
+        x_learn = np.array([[-5,7], [-4, 5], [-1, 12], [0, 11], [3, -1], [7, -10], [4, -6], [12, -7]])
+        y_learn = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+        lin_alg = LogisticCustom(x_learn,y_learn)
         reg_func = lin_alg.regression()
         
         x_test = np.array([[-6, 12], [-10, 10], [-7, 11], [-42, 21],[-1.5, 15], [-2, 23]])
