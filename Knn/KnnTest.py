eb23b3bda173a20ad404e422541d6bdf4b88f3f5
Andriy <andriy.rom1294@gmail.com>
2017-11-21 12:24:27 +0200

    Knn interface added with tests.

--- Knn/KnnTest.py
+++ Knn/KnnTest.py
@@ -4,4 +4,53 @@ Created on Tue Nov 21 11:38:44 2017
 
 @author: Andriy
 """
+import unittest
+import numpy as np
+from knn import knn
 
+class KnnTest(unittest.TestCase):
+    def testOneClass(self):
+        start = 9
+        count = 7
+        x_learn = np.random.normal(start, 2, size = (count, 2))
+        y_learn = np.zeros(count)
+        alg = knn(x_learn, y_learn) 
+        
+        neighbors = 3
+        test_x = [7,7]
+        test_y = 0
+        res_y = alg.classify(test_x, neighbors)
+        np.testing.assert_equal(res_y, test_y)
+        
+        test_x = [-3,-3]
+        test_y = 0
+        res_y = alg.classify(test_x, neighbors)
+        np.testing.assert_equal(res_y, test_y)
+        
+        
+    def testTwoClasses(self):
+        start = 9
+        count = 7
+        claster1 = np.random.normal(start, 2, size = (count, 2))
+        clas1 = np.zeros(count)
+        start = -5
+        claster2 = np.random.normal(start, 2, size = (count, 2))
+        clas2 = np.ones(count)
+        x_learn = np.vstack((claster1, claster2))
+        y_learn = np.vstack((clas1, clas2))
+        alg = knn(x_learn, y_learn) 
+        
+        neighbors = 3
+        test_x = [7,7]
+        test_y = 0
+        res_y = alg.classify(test_x, neighbors)
+        np.testing.assert_equal(res_y, test_y)
+        
+        test_x = [-3,-3]
+        test_y = 1
+        res_y = alg.classify(test_x, neighbors)
+        np.testing.assert_equal(res_y, test_y)
+
+if __name__ == "__main__":
+    tests = unittest.TestLoader().loadTestsFromTestCase(KnnTest)
+    unittest.TextTestRunner(verbosity=2).run(tests)
--- Knn/knn.py
+++ Knn/knn.py
@@ -4,4 +4,13 @@ Created on Tue Nov 21 11:37:33 2017
 
 @author: Andriy
 """
+import numpy as np
 
+class knn:
+    #x_train - numpy.array, dim=2; y_train - numpy.array, dim=1;
+    def __init__(self, x_train, y_train):
+        self.__x = x_train.copy()
+        self.__y = y_train.copy()
+    
+    def classify(self, point, neighbors_count):
+        return -1
