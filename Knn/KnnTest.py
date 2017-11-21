03797b1497ab3d3e5dabf1fae1fae2e49d8996e1
Andriy <andriy.rom1294@gmail.com>
2017-11-21 11:40:25 +0200

    Added files for Knn algorithm.

---
+++ Knn/KnnTest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 11:38:44 2017
+
+@author: Andriy
+"""
+
---
+++ Knn/knn.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 11:37:33 2017
+
+@author: Andriy
+"""
+
